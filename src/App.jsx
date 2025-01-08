2f69d75f674686b13062eea7a8a4aad1ee2d492a
RogersSC <ssras@gmail.com>
2025-01-07 21:37:43 -0400

    Laboratorio 1

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,8 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <h1>Diplomado Full Stack</h1>
+      <h2>Curso de React </h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
