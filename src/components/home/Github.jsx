d495eb5fca4d83f1a6571007f5ec0921298c83d0
RogersSC <ssras@gmail.com>
2025-01-08 21:46:10 -0400

    props

---
+++ src/components/home/Github.jsx
@@ -0,0 +1,14 @@
+import gitLogo from "../../assets/descarga.png";
+const Github = () => {
+    return(
+        <div>
+            <div>
+                <a href="https://github.com/svigabriel/frontendReactjs" target="_blank">
+                  <img src={gitLogo} className="git react" alt="Git logo" />
+                </a>
+             </div>
+        </div>     
+        
+    )
+};
+export default Github;
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,15 +1,28 @@
-import Logo from '../components/home/logo.jsx';
+import OpenLink from "../components/Core/OpenLink.jsx";
 import Body from '../components/home/body.jsx';
 import Footer from '../components/home/footer.jsx';
 
+import viteLogo from "../assets/vite.svg";
+import reactLogo from "../assets/react.svg";
+import githubLogo from "../assets/descarga.png";
 const Home = () => {
   return (
       <div>
-          <Logo />
+          <OpenLink
+              redirectURL="https://vite.dev"
+              logoImage={viteLogo}
+          />
+<OpenLink
+              redirectURL="https://react.dev"
+              logoImage={reactLogo}
+          />
+          <OpenLink
+              redirectURL="https://github.com/svigabriel/frontendReactjs"
+              logoImage={githubLogo}
+          />
           <Body />
           <Footer />
       </div>
   );
-};
-
+}
 export default Home;
