11931573d11c9af5f2641ff10d4db0ec1f8dfd8f
RogersSC <ssras@gmail.com>
2025-01-14 22:08:33 -0400

    Practica + Laboratorio + estados

--- src/screens/About.jsx
+++ src/screens/About.jsx
@@ -1,12 +1,28 @@
 import { useSelector } from 'react-redux'
+import { useState } from 'react';
 
 const About = () => {
-    const count = useSelector((state) => state.product.initialValue)
+    //los hooks en react son funciones ya definidas
+    //los hooks es siempre tienen el prefijo use
+    //hook useState, use Effect, use Context, use Ref, use Transition
+    //es posible crear nuetros propios hooks: useService, useImage, useForm
+    //el hook useState me permite manejar variaable de estado
+    const [ nameComponent, setNameComponent ] = useState('About');
+
+    const count = useSelector((state) => state.product.initialValue);
+ 
+    //const product = useSelector((state) => state.product.dataProduct)
     return (
         <>
-            <p>Component About</p>
-            Valor de InitialValue es <span>{ count }</span>
+            <p>Component {nameComponent} </p>
+            
+            <button onClick={() => {setNameComponent('Curso de React')}} />
+<button onClick={() => {setNameComponent('Curso de React')}}>Press</button>
+            <span>Valor de InitialValue es { count }</span>
+            <br />
+            
         </>
     );
 }
 export default About;
+
