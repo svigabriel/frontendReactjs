edaa87d1b919481f4986effefc78a13b9673095e
RogersSC <ssras@gmail.com>
2025-01-23 22:33:28 -0400

    Examen Final hasta ejercicio 8

--- src/screens/Think.jsx
+++ src/screens/Think.jsx
@@ -1,7 +1,14 @@
+import Home from '../screens/Home.jsx'
+
 const Think = () => {
     return (
         <>
             <p>Component Think</p>
+            <p>incluye Pagina Home</p>
+            <di>
+                <Home/>
+            </di>
+            
         </>
     );
 }
